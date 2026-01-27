abf11b6954b9e0c4c43e984658f8d98ed9b8cd4d
Asliddin567 <fazliddingabdurahmatov6@gmail.com>
2026-01-27 14:18:46 +0500

    regisetr

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Talents from './pages/Talent';
-import Jobs from './pages/Jobs';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Talents from "./pages/Talent";
+import Jobs from "./pages/Jobs";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen flex flex-col bg-gray-50">
-        <Header />
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" />
-            <Route path="/talents" element={<Talents />} />
-            <Route path="/jobs" element={<Jobs />} />
-            {/* <Route path="/signin" element={<SignIn />} /> */}
-            {/* <Route path="/signup" element={<SignUp />} /> */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </>
-  )
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<SignUpPage />} />
+          <Route path="/talents" element={<Talents />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
